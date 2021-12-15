eb279348830494466f320830cbca6bbfdff08bda
Mihas <32958803+MrMihas@users.noreply.github.com>
2021-12-15 23:54:12 +0200

    basic code without styles

---
+++ index.js
@@ -0,0 +1,48 @@
+ let out = document.querySelector(".out");
+ let text = document.querySelector(".text");
+  
+ let btn = document.querySelector(".btn");
+ let copy = document.querySelector(".copy");
+ 
+ btn.addEventListener("click", ()=>{
+     const search = " " + "\""; // попробовать тернарное выражение
+     const replaceWith = ' &laquo';
+     let str = " " + '<pre>' + text.value + "</pre>";
+     const result = str.replaceAll(search, replaceWith);
+     next(result);
+//  out.innerHTML = result;
+ 
+ })
+
+
+ 
+ function next(text){
+     search = '\"';
+     replaceWith = '&raquo';
+     result = text.replaceAll(search, replaceWith);
+     test(result);
+ }
+ 
+ 
+ function test(text){
+    var search = ['\n' + "&raquo"];
+    const replaceWith = '&laquo'
+    const result = text.replaceAll(search, replaceWith);
+    out.innerHTML = result;
+   
+ }
+
+
+ copy.addEventListener('click', function() {
+    let copyText = out.textContent || out.innerText;
+if(copyText == '')return
+    navigator.clipboard.writeText(copyText);
+    copy.setAttribute('value', 'Copied');
+    copy.setAttribute('disabled', 'true')
+    setTimeout(()=>{
+        copy.setAttribute('value', 'Copy')
+        copy.removeAttribute('disabled')
+    },2000)
+     })
+
+ 
